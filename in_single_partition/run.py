3cf5263924b79f7beca11befd17653951acd4070
Cloud User <centos@ip-10-11-229-31.ec2.internal>
2017-10-11 21:07:32 +0000

    add support for running scylla as a service and for throughput vs latency
    - add support for running test on a server that runs scylla as a service
    - add support for running test in two modes
      - latency - in which case the max_reqs is limited to make sure we are
        measuring latency when the system is not pressured
      - throughput - in which case max_reqs is not limited
    
    Signed-off-by: Cloud User <centos@ip-10-11-229-31.ec2.internal>

--- in_single_partition/run.py
+++ in_single_partition/run.py
@@ -2,7 +2,8 @@
 import subprocess
 
 
-clustering_row_count = 5*100000
+#clustering_row_count = 5*100000
+clustering_row_count = 20*100000
 clustering_row_size = 10
 
 def run_commands(commands):
@@ -11,29 +12,70 @@ def run_commands(commands):
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         p.wait()
 
+command_write = "scylla-bench -workload sequential -mode write -partition-count 1 -clustering-row-count %d -clustering-row-size %d  -concurrency 1  -nodes 127.0.0.1" % (clustering_row_count, clustering_row_size)
+
 def setup():
-    command_write = "scylla-bench -workload sequential -mode write -partition-count 1 -max-rate 1000 -clustering-row-count %d -clustering-row-size %d  -concurrency 1  -nodes 127.0.0.1" % (clustering_row_count, clustering_row_size)
     ccm_commands = ["ccm stop","ccm remove scylla-1","ccm create scylla-1 --scylla --vnodes -n 1 --install-dir=/home/shlomi/scylla","ccm start --wait-for-binary-proto", command_write, "ccm flush", "ccm stop", "ccm start --wait-for-binary-proto"]
 #    ccm_commands = ["ccm stop","ccm remove scylla-1","ccm create scylla-1 --vnodes -n 1 --version 3.11.0","ccm start --wait-for-binary-proto", command_write, "ccm flush", "ccm stop", "ccm start --wait-for-binary-proto"]
+    run_commands(ccm_commands)
+
+def setup_service():
+    ccm_commands = ["sudo service syclla-server stop","sudo pkill -9 scylla","sudo rm -Rf /var/lib/scylla/data/*", "sudo rm -Rf /var/lib/scylla/commitlog/*","sleep 3","sudo service scylla-server start","sleep 60", command_write, "nodetool flush"]
+#    ccm_commands = ["sudo pkill -9 scylla","sudo pkill -9 java","sudo rm -Rf /var/lib/scylla/cassandra/data/*", "sudo rm -Rf /var/lib/scylla//cassandra/commitlog/*","sudo rm -Rf /var/lib/scylla//cassandra/saved_caches/*","sleep 3","cd ~/cassandra/apache-cassandra-3.11.0/; bin/cassandra","sleep 60"]
     run_commands(ccm_commands)
 
 def stop_start():
     ccm_stop_start_commands = [ "ccm stop", "ccm start --wait-for-binary-proto"]
     run_commands(ccm_stop_start_commands)
 
-def run_test():
-    setup()
+def stop_start_service():
+    ccm_stop_start_commands = [ "sudo service scylla-server stop", "sudo pkill -9 scylla", "sudo service scylla-server start", "sleep 60"]
+    run_commands(ccm_stop_start_commands)
 
-    for rows_per_request in [500,1000]:
-        stop_start()
-        print rows_per_request
-        concurrency = 1000/rows_per_request
-        command_read = "scylla-bench -workload uniform -duration 5s -mode read -partition-count 1 -clustering-row-count %d -clustering-row-size %d  -concurrency %d -in-restriction -rows-per-request %d --nodes 127.0.0.1" % (clustering_row_count, clustering_row_size,concurrency,rows_per_request)
-        p = subprocess.Popen(command_read, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        for line in p.stdout.readlines():
-               print line
-        p.wait()
 
-    
+def run_a_test(rows_per_request,concurrency,max_reqs):
+    stop_start_service()
+    print "rows_per_request %d concurrency %d max_reqs %d" % (rows_per_request,concurrency,max_reqs)
+    command_read = "scylla-bench -workload uniform -duration 30s -mode read -partition-count 1 -clustering-row-count %d -clustering-row-size %d  -concurrency %d -in-restriction -rows-per-request %d --nodes 127.0.0.1" % (clustering_row_count, clustering_row_size,concurrency,rows_per_request)
+    if max_reqs > 0:
+       command_read = "%s -max-rate %d" % (command_read, max_reqs)
+    p = subprocess.Popen(command_read, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout.readlines():
+           print line
+    p.wait()
 
-run_test()
+
+def run_test_in_query_vs_split_query(limit):
+    print "run_test_in_query_vs_split_query limit rows %d" % limit
+    setup_service()
+
+    max_reqs = -1
+    if limit > 0:
+       max_reqs = limit
+
+    for concurrency in [1,2,3,4,5,10,25,50,100]:
+        run_a_test(1,concurrency,max_reqs);
+
+    for rows_per_request in [2,3,4,5,10,25,50,100]:
+        max_reqs = -1
+        if limit > 0:
+            max_reqs = limit/rows_per_request
+        run_a_test(rows_per_request,1,max_reqs);
+
+def run_test_in_query_limited_concurrency(limit):
+    print "run_test_in_query_limited_concurrency limit rows %d" % limit
+    setup_service()
+ 
+
+    for rows_per_request in [1,2,3,4,5,10,25,50,100]:
+        max_reqs = -1
+        if limit > 0:
+            max_reqs = limit/rows_per_request
+        run_a_test(rows_per_request,10,max_reqs)
+
+
+run_test_in_query_limited_concurrency(2000)
+run_test_in_query_limited_concurrency(-1)
+
+run_test_in_query_vs_split_query(500)
+run_test_in_query_vs_split_query(-1)
